c8e8379694d5f9dc55c893cad440ffbb59067f86
Praharsh <praharsh_bc22070@srmcem.ac.in>
2026-08-23 14:53:06 +0530

    Add dockerfile

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,12 +4,16 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const frontendUrl = process.env.FRONTEND_URL ;
+
+const publicDir = path.join(process.cwd(), 'public');
 
 app.use(express.json());
 app.use(cors({
@@ -21,6 +25,15 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "Server is healthy anna vannakam" });
 });
+
+
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir));
+    
+    app.get("/{*any}", (req,res,next) =>{
+        res.sendFile(path.join(publicDir, 'index.html') , (err) => next(err));
+    })
+}
 
 app.listen(port, ()=>{
     connectDB();
